13fe8ee402d303fac02114fc6535d92825eb303a
Kerollos Magdy <36763164+kerolloz@users.noreply.github.com>
2018-11-20 07:29:09 +0200

    refactoring
     change variables names from handles to comments :wink:

--- comments-parser.py
+++ comments-parser.py
@@ -1,12 +1,12 @@
 import re
 
-file = open('comments.txt', 'r')
+file = open('comments_html.txt', 'r')
 comments_html = file.read()
-handles = re.findall('<span class="UFICommentBody"><span>(.*?)</span>', comments_html)
+comments = re.findall('<span class="UFICommentBody"><span>(.*?)</span>', comments_html)
 
-print(len(handles))
-with open('handles_special_training.txt', 'w') as out_file:
-    for handle in handles:
-        out_file.write("%s, " % handle)
-        print(handle)
+print(len(comments))
+with open('parsed_comments.txt', 'w') as out_file:
+    for comment in comments:
+        out_file.write("%s, " % comment)
+        print(comment)
 
